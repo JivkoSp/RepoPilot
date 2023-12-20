c53635ba7dfec6e3fa5ff1d50355e124c425d3fe
JivkoSp <atjspasov@gmail.com>
2023-12-20 17:02:36 +0300

    Added Services/GitHubService.cs

---
+++ Services/GitHubService.cs
@@ -0,0 +1,44 @@
+﻿using Octokit;
+
+namespace RepoPilot.Services
+{
+    internal sealed class GitHubService
+    {
+        private const string TokenFilePath = "github_token.txt";
+
+        public static string GetStoredGitHubToken()
+        {
+            return File.Exists(TokenFilePath) ? File.ReadAllText(TokenFilePath).Trim() : "";
+        }
+
+        public static async Task SetGitHubCredentials()
+        {
+            Console.Write("Enter your GitHub personal access token: ");
+            string? token = Console.ReadLine()?.Trim();
+
+            if (string.IsNullOrEmpty(token))
+            {
+                Console.WriteLine("Token cannot be empty.");
+                return;
+            }
+
+            var githubClient = new GitHubClient(new ProductHeaderValue("RepoPilot"))
+            {
+                Credentials = new Credentials(token)
+            };
+
+            try
+            {
+                var user = await githubClient.User.Current();
+
+                Console.WriteLine($"Authenticated as {user.Login}");
+
+                File.WriteAllText(TokenFilePath, token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Authentication failed: {ex.Message}");
+            }
+        }
+    }
+}
