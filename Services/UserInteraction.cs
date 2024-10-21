1a6e859eeaaa391177b3ba7a6001110522063757
JivkoSp <atjspasov@gmail.com>
2024-10-21 14:10:39 +0300

    Updated the DisplayPilotHelp method in Services/UserInteraction.cs

--- Services/UserInteraction.cs
+++ Services/UserInteraction.cs
@@ -16,11 +16,11 @@ namespace RepoPilot.Services
             Console.WriteLine("  cd <directory>         - Change directory");
             Console.WriteLine("  mkdir <dir>            - Create a new directory");
             Console.WriteLine("  look <file>            - Display file information");
-            Console.WriteLine("  clear                  - Clear the console");
             Console.WriteLine("  git <args>             - Run git commands");
             Console.WriteLine("  pilot <args>             - Run pilot commands");
             Console.WriteLine("  alias <name> <command> - Create an alias");
             Console.WriteLine("  help                   - Display this help message");
+            Console.WriteLine("  clear                  - Clear the console");
             Console.WriteLine("  exit                   - Exit the shell");
             Console.WriteLine();
         }
@@ -31,7 +31,6 @@ namespace RepoPilot.Services
             Console.WriteLine("  pilot create          - Create a new GitHub repository and optionally initialize it locally");
             Console.WriteLine("  pilot manage branches - Manage branches in the local repository");
             Console.WriteLine("  pilot manage commits  - Manage commits in the local repository");
-            Console.WriteLine("  help                  - Display this help message");
             Console.WriteLine();
         }
 
